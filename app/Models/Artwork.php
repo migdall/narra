84f44c66096992b98c78c246abe79b28a4ac8f25
Jesus Noland <jesus.noland@litpen.com>
2026-06-01 10:22:24 -0700

    Fix bugs with the seeder

--- app/Models/Artwork.php
+++ app/Models/Artwork.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Artwork extends Model
 {
-    //
+    use HasFactory; // <-- This fixes your factory() error
+
+    /**
+     * The attributes that are mass assignable.
+     * <-- This prevents the Mass Assignment error when running the seeder
+     */
+    protected $fillable = [
+        'dams_id',
+        'title',
+        'artist',
+        'description',
+        'image_url',
+        'is_published',
+    ];
 }
--- database/seeders/ArtworkSeeder.php
+++ database/seeders/ArtworkSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Artwork;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
